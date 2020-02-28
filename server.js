00e4226092e661c4c3dd9ac9c9b50359f4f60dba
Ida Onshus <Ida.Onshus@nav.no>
2020-02-28 20:49:54 +0100

    legger til oppgaver

--- server.js
+++ server.js
@@ -1,35 +1,17 @@
 const express = require('express');
 const server = express();
-const port = 3000;
-var path = require('path');
-var helmet = require('helmet')
+const port = 1337;
+const helmet = require('helmet');
 
-
-
-/*CSP*/
+// ------ CSP -------
+/*
+server.use(helmet());
 server.use(helmet.contentSecurityPolicy({
     directives: {
-        defaultSrc: ['\'none\''],
-        mediaSrc: ['\'self\''],
-        scriptSrc: ['\'self\'', 'https://www.google-analytics.com'],
-        objectSrc: ['\'none\''],
-        frameAncestors: ['\'none\''],
-        formAction: ['\'self\''],
-        styleSrc: ['\'self\''],
-        fontSrc: ['\'self\'', 'data:', 'https://fonts.gstatic.com'],
-        imgSrc: ['\'self\'', 'data:', 'https://www.google-analytics.com'],
-        connectSrc: ['\'self\'', 'https://www.google-analytics.com'],
-        frameSrc: ['\'self\'', 'https://video.qbrick.com']
+        defaultSrc: ["'none'"],
     }
 }));
-
-/*Start server*/
+*/
 
 server.use(express.static('src'));
-server.use('/public', express.static('src'));
-
-server.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
-
 server.listen(port);
---
+++ src/index.js
@@ -0,0 +1,34 @@
+setInterval(() => {
+    document.getElementById('insertfromscript').innerHTML = 'Du fikk kjørt javascript klokka ' + new Date().toLocaleTimeString();
+}, 1000);
+
+const randomnr = Math.floor(Math.random() * 100);
+fetch('https://ironhack-pokeapi.herokuapp.com/pokemon/' + randomnr)
+    .then(reponse => reponse.json())
+    .then(data => {
+        const pokemanNameElement = document.getElementById('pokemonname');
+        const pokemanImgElement = document.getElementById('pokemonimg');
+        pokemanNameElement.innerHTML = data.ename + ' ' + data.cname;
+        pokemanImgElement.src = data.picture;
+    });
+
+
+
+function loop() {
+    var s = '',
+        e = ['🤓', '😚', '😘'],
+        i, m;
+
+    for (i = 0; i < 10; i ++) {
+        m = Math.floor(e.length * ((Math.sin((Date.now()/200) + i)+1)/2));
+        s += e[m];
+    }
+
+    location.hash = s;
+
+    setTimeout(loop, 50);
+}
+
+window.addEventListener('load', function () {
+    loop();
+});
