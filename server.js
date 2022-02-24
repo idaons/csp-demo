a00f115581d88d7b51ef3ad57285f18c9f466565
Ida Onshus <25682301+idaons@users.noreply.github.com>
2022-02-24 16:26:44 +0100

    rydder

--- server.js
+++ server.js
@@ -8,12 +8,6 @@ server.use(helmet());
 server.use(helmet.contentSecurityPolicy({
     directives: {
       defaultSrc: ["'none'"],
-      imgSrc: ["'self'", 'ironhack-pokeapi.herokuapp.com'],
-      connectSrc: ['ironhack-pokeapi.herokuapp.com'],
-      fontSrc: ['fonts.gstatic.com'],
-      frameSrc: ['www.youtube.com'],
-      styleSrc: ["'self'",'fonts.googleapis.com', "'sha256-1AEBufVZt2FioEw++D/tRmJhuJ1S/f9eBmgaVZR52Dg='"],
-      scriptSrc: ["'self'"],
 }
 }));
 
